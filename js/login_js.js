3b659fb786835e8aea399c541e77cd9bf69f8fcc
Rafeedmefood <rafidkarim36@gmail.com>
2022-01-10 13:19:14 -0500

    "Completed login page"

--- js/login_js.js
+++ js/login_js.js
@@ -3,9 +3,21 @@ let buttonEl = document.getElementById("button")
 
 
 buttonEl.addEventListener("click", function() {
+
     let password = document.getElementById("the-pass").value
+    let userName = document.getElementById("the-username").value
+    let email = document.getElementById("the-email").value
+    let messageEl = document.getElementById("message")
+
     document.getElementById("the-pass").value = ""
     document.getElementById("the-email").value = ""
     document.getElementById("the-username").value = ""
-    
+    if(userName === "" || email === "" ) {
+        messageEl.textContent = `Please fill out all fields!!`
+    }
+    else if (password == 12345678) {
+        messageEl.textContent = `${userName} DOES exist in the database!`
+    } else {
+        messageEl.textContent = `INCORRECT VALUES! This person does not exist in the database`
+    }
 })
